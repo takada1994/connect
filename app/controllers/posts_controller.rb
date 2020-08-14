55019c70028ce98c9741aa980bf96db1870f7d22
takada1994 <takadanaritoshi@gmail.com>
2020-08-14 19:26:20 +0900

    投稿一覧ページと、投稿機能を追加。新規投稿を投稿一覧ページにリダイレクト。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,17 @@ class PostsController < ApplicationController
     @post = Post.find_by(id: params[:id])
   end
 
+  def new
+  end
+  
+  def create
+    @post = Post.new(content: params[:content])
+    @post.save
+    redirect_to("/posts/index")
+  end
+
+  def index
+    @posts = Post.all.order(created_at: :desc)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   get :login, to: "home#login" , as:"login"
 
   get "posts/:id" => "posts#show"
-
+  get :new, to: "posts#new"
+  post "posts/create" => "posts#create"
 end
